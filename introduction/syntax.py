b9fb8da5d37f857232c2d1774494707cc91b8bd1
Luan Oliveira <luanoliveira17@hotmail.com>
2025-04-25 16:40:36 -0300

    feat: add syntax

---
+++ introduction/syntax.py
@@ -0,0 +1,12 @@
+# One line comment
+"""
+  Multiple line comment
+"""
+
+# Python uses indentation (tabs) to create blocks of code, each tab represents that code is under a block
+age = 26
+
+if age < 18:
+  print("underage")
+else:
+  print("of legal age")
